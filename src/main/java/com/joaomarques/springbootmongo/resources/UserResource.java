79207e606a3f5ba5f4c23910a3847049dbf6f2a7
João Marques <jsmarques2011@gmail.com>
2024-03-04 11:51:01 +0000

    POST User

--- src/main/java/com/joaomarques/springbootmongo/resources/UserResource.java
+++ src/main/java/com/joaomarques/springbootmongo/resources/UserResource.java
@@ -1,17 +1,20 @@
 package com.joaomarques.springbootmongo.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joaomarques.springbootmongo.domain.User;
 import com.joaomarques.springbootmongo.dto.UserDTO;
 import com.joaomarques.springbootmongo.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -42,6 +45,16 @@ public class UserResource {
         User obj = service.findById(id);
         
         return ResponseEntity.ok().body(new UserDTO(obj));
+
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+
+        User obj = service.fromDTO(objDto);  //convert dto to user
+        obj = service.insert(obj);  //insert user in database
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId()).toUri();  //return with header
+        return ResponseEntity.created(uri).build();
 
     }
 }
--- src/main/java/com/joaomarques/springbootmongo/services/UserService.java
+++ src/main/java/com/joaomarques/springbootmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaomarques.springbootmongo.domain.User;
+import com.joaomarques.springbootmongo.dto.UserDTO;
 import com.joaomarques.springbootmongo.repository.UserRepository;
 import com.joaomarques.springbootmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,13 @@ public class UserService {
             throw new ObjectNotFoundException("Object not found");
         }
         return user.get();
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
