63f347bf988309352adce244eef4d3619d3fa604
João Marques <jsmarques2011@gmail.com>
2024-03-04 11:39:51 +0000

    Get user by id

--- src/main/java/com/joaomarques/springbootmongo/resources/UserResource.java
+++ src/main/java/com/joaomarques/springbootmongo/resources/UserResource.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -32,6 +33,15 @@ public class UserResource {
         //stream -> coleção compatível com expressões lamba do java
         //map -> pega em cada objeto x da lista original e retorna um novo DTO com esse objeto
         return ResponseEntity.ok().body(listDto);
+
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+        User obj = service.findById(id);
+        
+        return ResponseEntity.ok().body(new UserDTO(obj));
 
     }
 }
--- src/main/java/com/joaomarques/springbootmongo/services/UserService.java
+++ src/main/java/com/joaomarques/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.joaomarques.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaomarques.springbootmongo.domain.User;
 import com.joaomarques.springbootmongo.repository.UserRepository;
+import com.joaomarques.springbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        if(!user.isPresent()){
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return user.get();
     }
 }
